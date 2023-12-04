03f191446169dcd9b393a04382279161686966ec
sarki012 <60797267+sarki012@users.noreply.github.com>
2023-12-03 21:29:25 -0600

    Claw Picks up Cup and Drops it in Trash

--- visualServoing.py
+++ visualServoing.py
@@ -24,6 +24,17 @@ import queue
 
 def task3():
    # x= 0
+    startRight = 0
+    endRight = 0
+    movedRight = 0
+    startLeft = 0
+    endLeft = 0
+    movedLeft = 0
+    direction = 0
+    currentTime = 0
+    seconds = 0
+    secondsStart = 0
+
     print("Thread 3 started. Waiting for the signal....")
     while True:
         try:
@@ -34,13 +45,21 @@ def task3():
 
             ##############Left to Right##############################
             if streamThread.x > 497:
+                startRight = time.time()
                 while streamThread.x > 497:
                     rcThread.deviceUC1.write('}')   #Right
                     time.sleep(1)
+                endRight = time.time()
+                movedRight = endRight - startRight
+                direction = 0
             elif streamThread.x < 497:
+                startLeft = time.time()
                 while streamThread.x < 497:
                     rcThread.deviceUC1.write('{')   #Left
                     time.sleep(1)
+                endLeft = time.time()
+                movedLeft = endLeft - startLeft
+                direction = 1
             for j in range (0, 10):
               rcThread.deviceUC1.write('q')   #Stop (Break)
             print("Left-To-Right Goal Achieved!")
@@ -54,19 +73,51 @@ def task3():
             print("Goal Achieved!")
 
             #############Squeeze claw until feedback is > 6750
-            while feedbackThread.feedBack > 650:
+            while feedbackThread.feedBack > 400:
                 rcThread.deviceUC1.write('c')   #Claw closed
             for j in range (0, 10):
                 rcThread.deviceUC1.write('%')   #Stop claw
             print("Claw Squeezed!")
 
             ##########Lift the boom up#####################
+            for k in range (0, 10000):
+                rcThread.deviceUC2.write('u')   #Boom up
+            for j in range (0, 10):
+                rcThread.deviceUC2.write('@')   #Stop boom
+            print("Boom Up!")
             
+            #if direction == 0:
+            
+            ###########Rotate Left####################
+            secondsStart = time.time()
+            while seconds - secondsStart < 5:
+                rcThread.deviceUC1.write('{')   #Left
+                seconds = time.time()
+            for j in range (0, 10):
+              rcThread.deviceUC1.write('q')   #Stop (Break)
+            print("Left Goal Achieved!")
+ #           secondsStart = time.time()
+  #          if direction == 0:
+   #         while (seconds - secondsStart) + movedRight < 10:
+    #            rcThread.deviceUC1.write('{')   #Left
+     #           seconds = time.time()
+      #      elif direction == 1:
+       #       while (seconds - secondsStart) - movedLeft < 10:
+        #        rcThread.deviceUC1.write('{')   #Left
+         #       seconds = time.time()
 
+
+            #############Open claw until feedback is > 600
+            while feedbackThread.feedBack < 600:
+                rcThread.deviceUC1.write('n')   #Claw open
+            for j in range (0, 10):
+                rcThread.deviceUC1.write('%')   #Stop claw
+            print("Claw Open!")
+
+        #    for m in range(0, 100): 
+         #     rcThread.client_sock.send('K')           #Send 'K' for clear to clear the go flag. We don't want the Android to keep sending 'go'
             robotMain.go_event.clear()        #Stop auto mode. This thread will wait until go button is pressed again.
-            for m in range(0, 10): 
-              rcThread.client_sock.send('K')           #Send 'K' for clear to clear the go flag. We don't want the Android to keep sending 'go'
-          
+
         except KeyboardInterrupt:
             print("\nDisconnected")
             raise SystemExit 
